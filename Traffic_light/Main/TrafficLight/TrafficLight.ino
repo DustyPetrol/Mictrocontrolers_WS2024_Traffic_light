e9d91089e9f327fb4e3ec1382a9162d26600d80f
Dusty <petr.lavrenov@stud.hshl.de>
2024-11-20 14:26:49 +0100

    remade Diagrams

--- Traffic_light/Main/TrafficLight/TrafficLight.ino
+++ Traffic_light/Main/TrafficLight/TrafficLight.ino
@@ -1,20 +1,16 @@
-#include <Arduino.h>    // Include Arduino library for basic functions
+#include <Arduino.h>    
 
-// Define pins for button and lights
-#define buttonPin 8                // Button to trigger pedestrian crossing
-#define trafficRedPin 13           // Traffic red light pin
-#define trafficYellowPin 12        // Traffic yellow light pin
-#define trafficGreenPin 11         // Traffic green light pin
-#define pedestrianRedPin 10        // Pedestrian red light pin
-#define pedestrianGreenPin 9       // Pedestrian green light pin
+#define buttonPin 8                
+#define trafficRedPin 13           
+#define trafficYellowPin 12        
+#define trafficGreenPin 11        
 
-// Define timing constants (in milliseconds)
-const unsigned long redTime = 5000;        // Duration of red light
-const unsigned long yellowTime = 2000;     // Duration of yellow light
-const unsigned long greenTime = 5000;      // Duration of green light
-const unsigned long redYellowTime = 1000;  // Duration of red + yellow light
-const unsigned long pedestrianGreenTime = redTime;   // Duration of pedestrian green light
 
+const unsigned long redTime = 5000;        
+const unsigned long yellowTime = 2000;    
+const unsigned long greenTime = 5000;     
+const unsigned long redYellowTime = 1000;  
+const unsigned long pedestrianGreenTime = redTime;   
 // Define states for traffic light control
 #define GREEN_LIGHT 0
 #define YELLOW_LIGHT 1
@@ -22,19 +18,21 @@ const unsigned long pedestrianGreenTime = redTime;   // Duration of pedestrian g
 #define RED_YELLOW_LIGHT 3
 #define PEDESTRIAN_CROSSING 4
 #define YELLOW_TO_RED 5
+#define MessageToSend 100
+#define MessageToRecieve 200
 
-// Traffic light class to control the light states and transitions
+
 class TrafficLight {
   private:
-    int redPin, yellowPin, greenPin;          // Pins for red, yellow, green lights
-    unsigned long previousMillis;             // Timestamp for timing control
-    int state;                                // Current state of the traffic light
-    bool pedestrianWaiting;                   // Flag for pedestrian request
-    uint8_t ReceavedMessage;                  // Stores received serial message
-    bool IsThereAMessage = false;             // Flag indicating if a message is to be sent
+    int redPin, yellowPin, greenPin;         
+    unsigned long previousMillis;             
+    int state;                               
+    bool pedestrianWaiting;                  
+    uint8_t ReceavedMessage=0;                  
+    bool IsThereAMessage = false;             
     
   public:
-    // Constructor to initialize traffic light pins and initial state
+   
     TrafficLight(int rPin, int yPin, int gPin) : 
       redPin(rPin), 
       yellowPin(yPin), 
@@ -45,34 +43,26 @@ class TrafficLight {
       pinMode(redPin, OUTPUT);
       pinMode(yellowPin, OUTPUT);
       pinMode(greenPin, OUTPUT);
-      redLight();   // Initialize with red light on
+      redLight();   
     }
 
-    // Update function to manage state transitions based on timing and pedestrian request
+    
     void update(unsigned long currentMillis) {
-      // Check if a serial message is available
-      if (Serial.available() > 0) {
-        ReceavedMessage = Serial.read();
-      } else {
-        ReceavedMessage = 0;
-      }
-
-      // Handle pedestrian crossing request
+      
       if (pedestrianWaiting && state != PEDESTRIAN_CROSSING && state != YELLOW_TO_RED) {
         if (state == RED_LIGHT) {
-          state = PEDESTRIAN_CROSSING;        // Switch to pedestrian crossing state
+          state = PEDESTRIAN_CROSSING;        
           previousMillis = currentMillis;
           pedestrianWaiting = false;
         } else {
-          state = YELLOW_TO_RED;              // Transition to yellow-to-red before pedestrian crossing
+          state = YELLOW_TO_RED;             
           previousMillis = currentMillis;
           pedestrianWaiting = false;
           yellowLight();
         }
       }
       
-
-      // State machine for traffic light control
+      
       switch (state) {
         case GREEN_LIGHT: 
           if (currentMillis - previousMillis >= greenTime) {
@@ -111,7 +101,7 @@ class TrafficLight {
           break;
         case PEDESTRIAN_CROSSING:
           SendMessage();
-          if (ReceavedMessage == 200) {
+          if (ReceavedMessage == MessageToRecieve) {
             ReceavedMessage = 0;
             state = RED_YELLOW_LIGHT;
             previousMillis = currentMillis;
@@ -122,20 +112,26 @@ class TrafficLight {
       }
     }
 
-    // Function to set message flag when a message needs to be sent
+   
     void ThereIsAMessage() {
       IsThereAMessage = true;
     }
 
-    // Send a message through Serial if the flag is set
+   void recieveMessage(uint8_t message){
+    ReceavedMessage=message;
+   }
+
+
+
+   
     void SendMessage() {
       if (IsThereAMessage) {
-        Serial.write(100);
+        Serial.write(MessageToSend);
         IsThereAMessage = false;
       }
     }
 
-    // Functions to set the traffic light color by controlling respective pins
+    
     void redLight() {
       digitalWrite(redPin, HIGH);
       digitalWrite(yellowPin, LOW);
@@ -160,51 +156,57 @@ class TrafficLight {
       digitalWrite(greenPin, LOW);
     }
 
-    // Trigger pedestrian crossing request
+    
     void triggerPedestrianCrossing() {
       pedestrianWaiting = true;
     }
 
-    // Check if the light is red
+    
     bool isRed() {
       return state == RED_LIGHT;
     }
 
-    // Check if pedestrian crossing state is active
+   
     bool isPedestrianCrossing() {
       return state == PEDESTRIAN_CROSSING;
     }
 };
 
-// Traffic system class to handle the traffic light and button for pedestrian crossing
+
 class TFSystem {
   private:
     TrafficLight traffic;
+    uint8_t MessageToPass;
 
   public:
     TFSystem(int trafficRed, int trafficYellow, int trafficGreen) 
     : traffic(trafficRed, trafficYellow, trafficGreen) {
-      pinMode(buttonPin, INPUT_PULLUP);  // Set button pin as input with pull-up resistor
+      pinMode(buttonPin, INPUT_PULLUP); 
     }
 
     void update(unsigned long currentMillis) {
       if (digitalRead(buttonPin) == LOW && !traffic.isPedestrianCrossing()) {
-        traffic.ThereIsAMessage();          // Set message flag
-        traffic.triggerPedestrianCrossing(); // Trigger pedestrian crossing
+        traffic.ThereIsAMessage();          
+        traffic.triggerPedestrianCrossing(); 
       }
+       while (Serial.available() > 0) {
+        MessageToPass=Serial.read();
+        traffic.recieveMessage(MessageToPass);
+      } 
 
-      traffic.update(currentMillis);        // Update traffic light based on current time
+
+      traffic.update(currentMillis);        
     }
 };
 
-// Instantiate the traffic system with defined traffic light pins
+
 TFSystem trafficSystem(trafficRedPin, trafficYellowPin, trafficGreenPin);
 
 void setup() {
-  Serial.begin(9600);    // Initialize serial communication
+  Serial.begin(9600);    
 }
 
 void loop() {
-  unsigned long currentMillis = millis();    // Get current time
-  trafficSystem.update(currentMillis);       // Update traffic system based on time
+  unsigned long currentMillis = millis();   
+  trafficSystem.update(currentMillis);       
 }
